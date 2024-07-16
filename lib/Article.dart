d090f39e17a25e25b3ee084a2794c58def5b2a17
Joseph Watunu <justinwatunu@gmail.com>
2024-07-16 21:48:07 +0300

    update article section
    adding styling to single article section

--- lib/Article.dart
+++ lib/Article.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:news_app/Constants/decorations.dart';
 
 class Article extends StatelessWidget{
   @override
@@ -8,16 +9,20 @@ class Article extends StatelessWidget{
       home: Scaffold(
         backgroundColor: Colors.grey[300],
         body: Center(
-          child: SingleChildScrollView(
-            child:Column (
-            children: [
-              ImageSection(image: 'images/plane.jpg'),
-              SizedBox(height: 10,),
-              AuthorSectionContext(name: 'name', date: 'date'),
-              SizedBox(height: 10,),
-              TextSection(description: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable. If you are going to use a passage of Lorem Ipsum,'
-                  ' you need to be sure there isnt anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.')
-            ],
+          child: SafeArea(
+            child: SingleChildScrollView(
+              child:Column (
+              children: [
+                SizedBox(height: 25,),
+                ImageSection(image: 'images/plane.jpg'),
+                SizedBox(height: 10,),
+                ArticleTitle(title: 'Contact Lost with KenyanAir Boeing 737-500 Moments After Take Off'),
+                AuthorSectionContext(name: 'name', date: 'date'),
+                SizedBox(height: 5,),
+                TextSection(description: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable. If you are going to use a passage of Lorem Ipsum,'
+                    ' you need to be sure there isnt anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.')
+              ],
+              ),
             ),
           ),
         ),
@@ -26,6 +31,22 @@ class Article extends StatelessWidget{
   }
 }
 
+
+class ImageSection extends StatelessWidget{
+  const ImageSection({super.key, required this.image});
+
+  final String image;
+
+  @override
+  Widget build (BuildContext context){
+    return Container(
+      height: 250,
+      width: 380,
+      decoration: BoxDecoration(image: DecorationImage(image: AssetImage('images/plane.jpg') ,fit: BoxFit.cover),
+        borderRadius: BorderRadius.circular(25),),
+    );
+  }
+}
 
 class AuthorSectionContext extends StatelessWidget{
   const AuthorSectionContext({
@@ -74,6 +95,26 @@ class AuthorSectionContext extends StatelessWidget{
   }
 }
 
+class ArticleTitle extends StatelessWidget{
+  const ArticleTitle({super.key, required this.title});
+  final String title;
+  
+  
+  @override
+  Widget build(BuildContext context){
+    return Container(
+      padding: EdgeInsets.all(20.0),
+      height: 150,
+      child: Align(
+        alignment: Alignment.center,
+        child:  Text(title, style: mainTitleStyle,),
+      )
+    );
+  }
+}
+
+
+
 class TextSection extends StatelessWidget{
   const TextSection({
     super.key,
@@ -93,18 +134,4 @@ class TextSection extends StatelessWidget{
   }
 }
 
-class ImageSection extends StatelessWidget{
-  const ImageSection({super.key, required this.image});
 
-  final String image;
-
-  @override
-  Widget build (BuildContext context){
-    return Image.asset(
-      image,
-      width: 600,
-      height: 240,
-      fit: BoxFit.cover,
-    );
-  }
-}
