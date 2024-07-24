ee85f499ee890e9c40319b3ac587608997d6e069
Joseph Watunu <justinwatunu@gmail.com>
2024-07-24 19:26:03 +0300

    create structure for API call
    creating the body that will be used to call API

---
+++ lib/Constants/newsApi.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Newsapi{
+   static const baseurl = "https://newsapi.org/v2/top-headlines?";
+   static const apikey= "4e3c52af12a94c598b6046e3e71d73e6";
+
+   Future<news> getNews() async{
+     final response= await http .get(Uri.parse(baseurl));
+
+     if(response.statusCode == 200){
+       return news.fromJson(jsonDecode(response.body)as Map<String, dynamic>);
+     }
+     else{
+       throw Exception('Failed to get from server');
+     }
+   }
+}
+
+class news{
+
+  final String author;
+  final String title;
+  final String imageUrl;
+  final String content;
+  final String date;
+
+  const news({
+    required this.author,
+    required this.title,
+    required this.imageUrl,
+    required this.content,
+    required this.date,
+  });
+
+  factory news.fromJson(Map<String, dynamic> json) {
+    return switch (json){
+      {
+        'author': String author,
+        'title': String title,
+      'imageurl': String imageurl,
+      'content': String content,
+      'date': String date,
+    } => news(
+        author: author,
+        title: title,
+        imageUrl: imageurl,
+        content: content,
+        date: date,
+      ),
+    _ => throw const FormatException('Error occurred')
+    };
+  }
+}
