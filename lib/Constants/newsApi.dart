eb9108155ebe06991ff3c66c5fcf0af0ab1e34a8
Joseph Watunu <justinwatunu@gmail.com>
2024-07-24 20:07:44 +0300

    Update newsApi.dart
    correcting structure of API call

--- lib/Constants/newsApi.dart
+++ lib/Constants/newsApi.dart
@@ -7,11 +7,13 @@ class Newsapi{
    static const baseurl = "https://newsapi.org/v2/top-headlines?";
    static const apikey= "";
 
-   Future<news> getNews() async{
-     final response= await http .get(Uri.parse(baseurl));
+   Future<List<news>> getNews() async{
+     final response= await http .get(Uri.parse('${baseurl}apikey=$apikey&country=us'));
 
      if(response.statusCode == 200){
-       return news.fromJson(jsonDecode(response.body)as Map<String, dynamic>);
+       List<dynamic> data = jsonDecode(response.body)['articles'];
+       print(data);
+       return data.map((article)=> news.fromJson(article)).toList();
      }
      else{
        throw Exception('Failed to get from server');
@@ -21,11 +23,11 @@ class Newsapi{
 
 class news{
 
-  final String author;
-  final String title;
-  final String imageUrl;
-  final String content;
-  final String date;
+  final String? author;
+  final String? title;
+  final String? imageUrl;
+  final String? content;
+  final String? date;
 
   const news({
     required this.author,
@@ -37,21 +39,12 @@ class news{
 
 
   factory news.fromJson(Map<String, dynamic> json) {
-    return switch (json){
-      {
-        'author': String author,
-        'title': String title,
-      'imageurl': String imageurl,
-      'content': String content,
-      'date': String date,
-    } => news(
-        author: author,
-        title: title,
-        imageUrl: imageurl,
-        content: content,
-        date: date,
-      ),
-    _ => throw const FormatException('Error occurred')
-    };
+    return news(
+      author: json['author'],
+      title: json['title'] ?? 'No title',
+      imageUrl: json['urlToImage'],
+      content: json['content']?? 'Content was null',
+      date: json['publishedAt'] ?? '',
+    );
   }
 }
